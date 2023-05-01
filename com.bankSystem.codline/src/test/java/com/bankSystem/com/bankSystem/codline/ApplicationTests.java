db678203ca35daf2114b1619e37478bb5d8563db
MarwaAlSinawi <116248656+MarwaAlSinawi@users.noreply.github.com>
2023-05-01 09:47:21 +0400

    first commit

---
+++ com.bankSystem.codline/src/test/java/com/bankSystem/com/bankSystem/codline/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankSystem.com.bankSystem.codline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
